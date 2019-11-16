27cf566023a37b71ecc269141168c3df1c146bf4
yamamotoyuuki <therizinosaurus514@gmail.com>
2019-11-16 22:24:31 +0900

    11/15 課題

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191108011959) do
+ActiveRecord::Schema.define(version: 20191115025136) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.string "picture"
   end
 
 end
